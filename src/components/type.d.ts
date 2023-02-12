265f326d624225476d5ff7fd3e2da89c7178a755
cxx <0001001bill@gmail.com>
2023-02-12 15:42:45 -0600

    refactor the publication items

---
+++ src/components/type.d.ts
@@ -0,0 +1,19 @@
+export interface DissertationType {
+    author: string[];
+    title: string;
+    school: string;
+    year: string;
+}
+
+export interface JournalType {
+    authors: string[];
+    title: string;
+    journal: string;
+    year: string;
+}
+
+export interface BookType {
+    authors: string[];
+    title: string;
+    year: string;
+}
