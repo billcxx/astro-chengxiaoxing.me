05345f088abc028f9199683a73ff66583c31af23
cxx <0001001bill@gmail.com>
2023-02-18 22:00:16 -0600

    update footer

--- src/content/config.ts
+++ src/content/config.ts
@@ -9,10 +9,8 @@ const mdSchema = z.object({
 
 // 2. Define your collection(s)
 const blogsCollection = defineCollection({ schema: mdSchema });
-const projectsCollection = defineCollection({ schema: mdSchema });
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
     'blogs': blogsCollection,
-    'projects': projectsCollection
 };
