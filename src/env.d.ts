2878a7a92ee85f9e3269544380b08c5a2a8f3d56
cxx <0001001bill@gmail.com>
2023-02-28 10:39:03 -0600

    sort blog posts by date

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,23 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+export interface DissertationType {
+    author: string[];
+    title: string;
+    school: string;
+    year: string;
+  }
+  
+  export interface JournalType {
+    authors: string[];
+    title: string;
+    journal: string;
+    year: string;
+  }
+  
+  export interface BookType {
+    authors: string[];
+    title: string;
+    year: string;
+  }
+  
---
+++ src/utils/sortPosts.ts
@@ -0,0 +1,5 @@
+import type { CollectionEntry } from 'astro:content';
+
+export function sortPosts(a: CollectionEntry<'blogs'>, b: CollectionEntry<'blogs'>) {
+    return b.data.publishDate.getTime() - a.data.publishDate.getTime()
+}
