66ac55e17e59e5c073e0914bc13089231848814c
cxx <0001001bill@gmail.com>
2023-03-02 10:55:31 -0600

    add sitemap and ads.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,10 @@ import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 
 // https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
 export default defineConfig({
   site: 'https://chengxiaoxing.me/',
-  integrations: [tailwind(), image()]
+  integrations: [tailwind(), image(), sitemap()]
 });
