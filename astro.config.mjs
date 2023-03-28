79bb6f521a0efb39b4449a002cfcb6823fe1374e
cxx <0001001bill@gmail.com>
2023-03-27 20:23:20 -0500

    remove svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,10 +9,7 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-import svelte from "@astrojs/svelte";
-
-// https://astro.build/config
 export default defineConfig({
   site: 'https://chengxiaoxing.me/',
-  integrations: [tailwind(),  sitemap(), svelte()]
+  integrations: [tailwind()]
 });
